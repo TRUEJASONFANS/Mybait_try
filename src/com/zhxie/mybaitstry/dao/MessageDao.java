d57bd9bacde0b4b131a9e41a30bc436db18b2f29
Xie Zhongwei <472408570@qq.com>
2016-03-01 23:44:19 +0800

    adding guava,adding the first mapper, and the domain

--- src/com/zhxie/mybaitstry/dao/MessageDao.java
+++ src/com/zhxie/mybaitstry/dao/MessageDao.java
@@ -1,10 +1,13 @@
 package com.zhxie.mybaitstry.dao;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 
+import com.google.common.collect.Lists;
 import com.zhxie.mybaitstry.db.DBAccess;
+import com.zhxie.mybaitstry.domain.Message;
 
 public class MessageDao {
 	
@@ -12,9 +15,15 @@ public class MessageDao {
 
 	public void queryMessageList(String command,String description) {
 		DBAccess dbAcess = new DBAccess();
+		List<Message> messageList = Lists.newArrayList();
 		try {
 			sqlSession = dbAcess.getSqlSession();
-		} catch (IOException e) {
+			Message message = new Message();
+			message.setCommand(command);
+			message.setDescription(description);
+			messageList = sqlSession.selectList("Message.queryMessageList",
+					message);
+		} catch (IOException e) { 
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
---
+++ src/com/zhxie/mybaitstry/domain/Message.java
@@ -0,0 +1,26 @@
+package com.zhxie.mybaitstry.domain;
+
+public class Message {
+	private String id;
+	private String command;
+	private String description;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getCommand() {
+		return command;
+	}
+	public void setCommand(String command) {
+		this.command = command;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
